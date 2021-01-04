1bba2e34c23702e4083a36f9027d59fe643a66a4
AkshajSaini <73529844+AkshajSaini@users.noreply.github.com>
2021-01-04 17:55:35 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,11 @@
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+  }
+  display(){
+    var pos =this.body.position;
+    rectMode(CENTER);
+   // fill("brown");
+    rect(pos.x, pos.y, this.width, this.height);
+  }
+};
